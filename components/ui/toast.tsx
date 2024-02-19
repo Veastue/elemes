67e5597c7b942bd53b433a468e7999f38617e458
bartolomecantiga <cantigaonduty@gmail.com>
2024-02-19 18:19:30 +0800

    Adding Course Creation Ui and edit Page and Course Title Form

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -27,7 +27,7 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "border bg-background text-foreground",
+        default: "border bg-emerald-700 text-white",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
       },
