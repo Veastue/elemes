c9a235b8ddde1fb30c1879af379035c0ed98e1b8
Veastue <cantigaonduty@gmail.com>
2024-02-27 12:36:36 +0800

    added correction

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,16 +4,16 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <main className='text-center h-screen w-screen flex flex-col justify-center items-center'>
+    <main className="text-center h-screen w-screen flex flex-col justify-center items-center">
         <Image
           src={'/4043.gif'}
-          alt='404 gif'
+          alt="404 gif"
           width={180}
           height={180}
         />
-        <h2 className='text-3xl'> It&apos;s not you, it&apos;s me!</h2>
+        <h2 className="text-3xl"> It's not you, it's me!</h2>
         <p>We could not find the page you are looking for</p>
-        <p>Go back to <Link href={'/'} className='font-bold text-rose-400 underline hover:underline-offset-0'>where we used to be.</Link></p>
+        <p>Go back to <Link href={'/'} className="font-bold text-rose-400 underline hover:underline-offset-0">where we used to be.</Link></p>
     </main>
   )
 }
